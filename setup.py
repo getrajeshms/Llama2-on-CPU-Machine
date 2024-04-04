54a5e687e29c0fa646b492176c6ef25a790840ba
Rajesh M S <38755175+getrajeshms@users.noreply.github.com>
2024-04-04 22:25:34 +0530

    update

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Bappy',
-    author_email= 'boktiar@ineuron.ai',
+    author= 'Rajesh M S',
+    author_email= 'getrajeshms@gmail.com',
     packages= find_packages(),
     install_requires = []
 
